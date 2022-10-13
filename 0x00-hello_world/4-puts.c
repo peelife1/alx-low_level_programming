aaa7e78f556614ad232143fa75c8216a999e2ee0
peelife1 <mbiakijepeter@gmail.com>
2022-10-13 15:24:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,9 @@
  * main- Entry
  * Return
  */
-
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		return (0);
 }
