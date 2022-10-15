dfaf3f24641e9fb2425692323e7e79f339012a16
peelife1 <mbiakijepeter@gmail.com>
2022-10-15 12:55:13 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - prints all strings from 0 to 9
+ *  Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	print("\n");
+
+	return (0);
+}
