b2ec1d4b190b810002d13c0361beb49516d98ab6
peelife1 <mbiakijepeter@gmail.com>
2022-10-15 15:41:57 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-
 /**
  *main-program entry point.
  *Return:0 no error, non zero if error
  */
 int main(void)
 {
-	char neel;
+	int i;
 
-	for (neel = '0'; neel <= '9'; neel++)
-	{
-		putchar(neel);
-	}
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
